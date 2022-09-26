421948c6dfb8503c71b79dbf817a792e02a1633d
Paweł Zmarlak <pawel.zmarlak@webcrafters.pl>
2022-09-26 11:39:59 +0200

    FIX - Add plugin info in the correct header

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,1 @@
 <?php
-/**
- * Plugin Name:       Open Graph Image Generator
- * Plugin URI:        https://github.com/slurmsmackenzie99/open-graph-image-generator
- * Description:       Generate Open Graph images.
- * Version:           1
- * Requires at least: 5.2
- * Requires PHP:      7.2
- * Author:            Pawel Zmarlak
- * Author URI:        https://webcrafters.pl/
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
- * Text Domain:       og-image-overlay
- * Domain Path:       /plugins
- */
